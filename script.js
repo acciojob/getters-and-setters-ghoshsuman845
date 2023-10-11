135d6fa9115ff20965cd9857ec9dcf8adb3b0918
AYUSHBANSAL-hub <72022069+AYUSHBANSAL-hub@users.noreply.github.com>
2023-10-11 16:18:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 class Person {
-  constructor(name, age) {
-    this._name = name;
-    this._age = age;
-  }
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-  get name() {
-    return this._name;
-  }
+    get name() {
+        return this._name;
+    }
 
-  set age(value) {
-    this._age = value;
-  }
+    set age(age) {
+        this._age = age;
+    }
 }
 
 class Student extends Person {
-  study() {
-    console.log(`${this._name} is studying.`);
-  }
+    study() {
+        console.log(`${this._name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-  teach() {
-    console.log(`${this._name} is teaching.`);
-  }
+    teach() {
+        console.log(`${this._name} is teaching`);
+    }
 }
 
-// Create a new instance of the Student class
-const student = new Student('John', 20);
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
 
-// Call the study method on the student instance
-student.study();
